ca15889ac4a41e1ffa8d0ec93afbce1336110eee
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-07-09 12:46:23 +0300

    Сделал первый паттерн

--- PatternStrategy/src/ru/ilyagutnikov/patterns/Main.java
+++ PatternStrategy/src/ru/ilyagutnikov/patterns/Main.java
@@ -3,8 +3,16 @@ package ru.ilyagutnikov.patterns;
 public class Main {
 
     public static void main(String[] args) {
-	Duck mallard = new MallardDuck();
+
+        Duck mallard = new MallardDuck();
         mallard.performFly();
         mallard.performQuack();
+
+
+        Duck model = new ModelDuck();
+        //поменял на лету полет
+        model.performFly();
+        model.setFlyBehavior(new FlyRocketPowered());
+        model.performFly();
     }
 }
