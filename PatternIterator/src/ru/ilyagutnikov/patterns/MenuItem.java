0e945eb6614fcb7ea296b0044d00d560e8ae883b
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-12-16 20:20:14 +0300

    Обновил гитигнор и начал делать паттерн итератор.

---
+++ PatternIterator/src/ru/ilyagutnikov/patterns/MenuItem.java
@@ -0,0 +1,41 @@
+package ru.ilyagutnikov.patterns;
+
+/**
+ * Created by IlyaGutnikov on 16.12.2016.
+ */
+public class MenuItem {
+
+    String name;
+    String description;
+    boolean vegeterian;
+    double price;
+
+    public MenuItem(String name, String description, boolean vegeterian, double price) {
+
+        this.name = name;
+        this.description = description;
+        this.vegeterian = vegeterian;
+        this.price = price;
+    }
+
+
+    public String getName() {
+
+        return name;
+    }
+
+    public String getDescription() {
+
+       return description;
+    }
+
+    public boolean isVegeterian() {
+
+        return vegeterian;
+    }
+
+    public double getPrice() {
+
+        return price;
+    }
+}
