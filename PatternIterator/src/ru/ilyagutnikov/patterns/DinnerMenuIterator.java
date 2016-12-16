de9678b72f512476b2253a3a83729da9b47429c9
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2016-12-16 21:19:13 +0300

    Начал реализовывать итератор.

---
+++ PatternIterator/src/ru/ilyagutnikov/patterns/DinnerMenuIterator.java
@@ -0,0 +1,35 @@
+package ru.ilyagutnikov.patterns;
+
+/**
+ * Created by IlyaGutnikov on 16.12.2016.
+ */
+public class DinnerMenuIterator implements Iterator {
+
+    MenuItem[] items;
+    int position = 0;
+
+    public DinnerMenuIterator(MenuItem[] items) {
+
+        this.items = items;
+    }
+
+    @Override
+    public boolean hasNext() {
+
+        if (position >= items.length || items[position] == null) {
+
+            return false;
+        } else {
+
+            return true;
+        }
+    }
+
+    @Override
+    public Object next() {
+
+        MenuItem menuItem = items[position];
+        position = position + 1;
+        return menuItem;
+    }
+}
---
+++ PatternIterator/src/ru/ilyagutnikov/patterns/Iterator.java
@@ -0,0 +1,10 @@
+package ru.ilyagutnikov.patterns;
+
+/**
+ * Created by IlyaGutnikov on 16.12.2016.
+ */
+public interface Iterator {
+
+    boolean hasNext();
+    Object next();
+}
