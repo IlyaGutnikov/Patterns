b893478fbc694934f2c6a396f88dbfacb5c53e40
Ilya Gutnikov <ilyagutnikov93@gmail.com>
2017-01-04 17:52:23 +0300

    Начал работать над патерном Компоновщик

---
+++ PatternIterator/src/ru/ilyagutnikov/patterns/MenuComponent.java
@@ -0,0 +1,39 @@
+package ru.ilyagutnikov.patterns;
+
+/**
+ * Created by IlyaGutnikov on 04.01.2017.
+ */
+public abstract class MenuComponent {
+
+    public void add(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void remove(MenuComponent menuComponent) {
+        throw new UnsupportedOperationException();
+    }
+
+    public MenuComponent getChild(int i) {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getName() {
+        throw new UnsupportedOperationException();
+    }
+
+    public String getDescription() {
+        throw new UnsupportedOperationException();
+    }
+
+    public double getPrice() {
+        throw new UnsupportedOperationException();
+    }
+
+    public boolean isVegeterian() {
+        throw new UnsupportedOperationException();
+    }
+
+    public void print() {
+        throw new UnsupportedOperationException();
+    }
+}
